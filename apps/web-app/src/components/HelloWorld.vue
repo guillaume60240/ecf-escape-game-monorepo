ce7301bc292bc9374b7cc747776c8d446e71e32f
Guillaume Lepoetre <guillaume.lepoetre@prestashop.com>
2023-04-02 19:14:21 +0200

    Add bootstrap and custom colors

--- apps/web-app/src/components/HelloWorld.vue
+++ apps/web-app/src/components/HelloWorld.vue
@@ -6,24 +6,37 @@ defineProps<{
 
 <template>
   <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
-    <h3>
+    <h1>{{ msg }}</h1>
+    <h3 class="text-primary">
       You’ve successfully created a project with
       <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
       <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
     </h3>
+    <i class="bi bi-2-circle-fill"></i>
   </div>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
 h1 {
   font-weight: 500;
   font-size: 2.6rem;
   top: -10px;
+  color: green;
+
+  &:hover {
+    color: greenyellow;
+  }
 }
 
 h3 {
   font-size: 1.2rem;
+}
+.greetings {
+  i {
+    font-size: 1.5rem;
+    margin-right: 0.5rem;
+    color: green;
+  }
 }
 
 .greetings h1,
--- apps/web-app/src/main.ts
+++ apps/web-app/src/main.ts
@@ -4,11 +4,12 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-import './assets/main.css'
+import './assets/main.scss'
+import 'bootstrap/dist/js/bootstrap.js'
+import 'bootstrap-icons/font/bootstrap-icons.css'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
 app.mount('#app')
