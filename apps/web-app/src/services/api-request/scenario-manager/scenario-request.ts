5a37ff956305019b7628afab98a6544998acfc55
Guillaume Lepoetre <guillaume.lepoetre@prestashop.com>
2023-04-23 18:42:41 +0200

    Create vue for scenario details

--- apps/web-app/src/services/api-request/scenario-manager/scenario-request.ts
+++ apps/web-app/src/services/api-request/scenario-manager/scenario-request.ts
@@ -14,4 +14,17 @@ export async function getAllScenarios(): Promise<scenarioDto[]> {
   return await response.json()
 }
 
-export default { getAllScenarios }
+export async function getOneScenarioById(id: string): Promise<scenarioDto> {
+  const response = await fetch(`${API_URl}/scenarios/${id}`, {
+    method: 'GET',
+    mode: 'cors',
+    cache: 'no-cache',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+  if (!response.ok) throw new Error('Scenario not found')
+  return await response.json()
+}
+
+export default { getAllScenarios, getOneScenarioById }
--- apps/web-app/src/views/HomeView.vue
+++ apps/web-app/src/views/HomeView.vue
@@ -9,24 +9,33 @@ import type { scenarioDto } from '@/dto/scenario.dto'
 import { getAllScenarios } from '@/services/api-request/scenario-manager/scenario-request'
 const state = reactive<{
   scenarios: scenarioDto[]
+  isLoading: boolean
 }>({
-  scenarios: []
+  scenarios: [],
+  isLoading: true
 })
 
-async function getSceanrios() {
+async function init() {
   state.scenarios = await getAllScenarios()
+  state.isLoading = false
 }
 
-getSceanrios()
+init()
 </script>
 
 <template>
   <main>
     <CarouselComponent />
-    <div class="row justify-content-around container mt-2">
+    <div class="row justify-content-around container mt-2" v-if="!state.isLoading">
       <template v-for="(scenario, index) in state.scenarios" :key="index">
         <WrapperComponent :scenario="scenario" class="col-12 col-lg-5 mt-4" />
       </template>
+    </div>
+    <div v-else class="mt-5 d-flex justify-content-center align-items-center flex-column">
+      <div class="spinner-border text-primary" role="status">
+        <span class="visually-hidden">Chargement...</span>
+      </div>
+      <p class="mt-3">Chargement de la page...</p>
     </div>
   </main>
 </template>
