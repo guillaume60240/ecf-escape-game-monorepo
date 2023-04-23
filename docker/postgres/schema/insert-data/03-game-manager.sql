6f094a1d477b825136db923e2157d85f48478d96
Guillaume Lepoetre <guillaume.lepoetre@prestashop.com>
2023-04-23 18:42:30 +0200

    Add data

---
+++ docker/postgres/schema/insert-data/03-game-manager.sql
@@ -0,0 +1,16 @@
+INSERT INTO "public"."game"("started_at", "ended_at", "duration", "booking_id", "scenario_id")
+VALUES
+(
+    '2023-04-21T10:00:16.409Z'::TIMESTAMP,
+    '2023-04-21T10:45:16.409Z'::TIMESTAMP,
+    '00:45:00'::TIME,
+    '1',
+    '1'
+),
+(
+    '2023-04-21T11:00:16.409Z'::TIMESTAMP,
+    '2023-04-21T11:47:16.409Z'::TIMESTAMP,
+    '00:47:00'::TIME,
+    '2',
+    '1'
+);
