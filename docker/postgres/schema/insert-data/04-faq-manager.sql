3fec6fef58e78ca8ab31c8e084c246a27924de97
Guillaume Lepoetre <guillaume.lepoetre@prestashop.com>
2023-05-08 15:38:43 +0200

    Update faq data. Update css profile view.

--- docker/postgres/schema/insert-data/04-faq-manager.sql
+++ docker/postgres/schema/insert-data/04-faq-manager.sql
@@ -2,10 +2,11 @@ INSERT INTO "public"."faq"("question", "answer")
 VALUES
 (
     'Comment puis-je réserver une partie d''escape game?',
-    'Vous pouvez réserver une partie d''escape game directement sur notre site web en sélectionnant le jour et l''heure qui vous conviennent le mieux. Vous pouvez également nous appeler pour effectuer votre réservation par téléphone. Veuillez noter que les réservations sont obligatoires et que nous ne pouvons garantir la disponibilité des créneaux horaires si vous ne réservez pas à l''avance.'
+    '<pre><code class="language-sql">Vous pouvez réserver une partie d''escape game directement sur notre site web en sélectionnant le jour et l''heure qui vous conviennent le mieux.
+Vous pouvez également nous appeler pour effectuer votre réservation par téléphone. Veuillez noter que les réservations sont obligatoires et que nous ne pouvons garantir la disponibilité des créneaux horaires si vous ne réservez pas à l''avance.</code></pre>'
 ),
 (
     ' Quel est le nombre de joueurs minimum et maximum pour une partie d''escape game ?',
-    'Le nombre de joueurs minimum et maximum varie en fonction de chaque jeu. Pour connaître le nombre de joueurs minimum et maximum pour un jeu en particulier, veuillez consulter la page de description de ce jeu sur notre site web. En général, nos jeux peuvent accueillir entre 2 et 8 joueurs. Si vous avez des questions sur le nombre de joueurs pour une partie d''escape game, n''hésitez pas à nous contacter.'
+    '<p>Le nombre de joueurs minimum et maximum varie en fonction de chaque jeu. Pour connaître le nombre de joueurs minimum et maximum pour un jeu en particulier, veuillez consulter la page de description de ce jeu sur notre site web. En général, nos jeux peuvent accueillir entre 2 et 8 joueurs. Si vous avez des questions sur le nombre de joueurs pour une partie d''escape game, n''hésitez pas à nous contacter.</p>'
 )
 ;
