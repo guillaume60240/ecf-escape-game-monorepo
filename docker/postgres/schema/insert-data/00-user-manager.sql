1bdbea975265f5de5dfa3f08a756fcdaaa57342f
Guillaume Lepoetre <guillaume.lepoetre@prestashop.com>
2023-05-21 17:13:36 +0200

    Update date for user-manager

--- docker/postgres/schema/insert-data/00-user-manager.sql
+++ docker/postgres/schema/insert-data/00-user-manager.sql
@@ -1,4 +1,3 @@
 INSERT INTO "public"."user"("name", "email", "password") 
 VALUES 
-    ('administrator', 'admincrypt@mail.fr', '$2b$10$SzeoH16m8177QIHMkBDzduzj7ABLz89ihIqN9wpfouA3iPv4dqj5O'),
-    ('Guillaume', 'guillaume.lepoetre@gmail.com', '$2b$10$SzeoH16m8177QIHMkBDzduzj7ABLz89ihIqN9wpfouA3iPv4dqj5O');
+    ('Studi', 'studi@mail.fr', '$2b$10$U.gKnRSVs1r2oSsk4eAReebLXBn7rA2GTfyQG1XP1GvtnduqlfR8i');
